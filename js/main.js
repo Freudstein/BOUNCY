fb14e98393fffa99fcf883e8c46bdf81dbf0e944
Alessio <prefetto.di.ferro@gmail.com>
2020-12-20 23:04:21 +0300

    Rest of the site

--- js/main.js
+++ js/main.js
@@ -1,11 +1,23 @@
 $(document).ready(function(){
 	$('.team__slider').slick({
 		arrows: false,
-		//speed: 2000,
-		//fade: true,
-		//autoplay: true,
 		infinite: true,
 		dots: true,
 		appendDots: $('.team__slider-nav'),
+	});
+
+	$('.testimonials__slider').slick({
+		arrows: false,
+		infinite: true,
+		dots: true,
+		appendDots: $('.testimonials__slider-nav'),
+	});
+
+	$('.blog__slider').slick({
+		vertical: true,
+		arrows: false,
+		infinite: true,
+		dots: true,
+		appendDots: $('.blog__slider-nav'),
 	});
 });
